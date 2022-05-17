188c735d68de8ed8457a4dccaf5172254ab2a2ff
Dandaran <105198675+Dandaran@users.noreply.github.com>
2022-05-17 13:00:31 +0200

    java2

--- JS/variablesjs.js
+++ JS/variablesjs.js
@@ -1,8 +1,17 @@
 // kebap-case (no permitido en JavaScript)
-const last-ame = 'Garoppo';
+const lastame = 'Garoppo';
 // snake_case (recomendado en otros lenguajes como Python)
 const last_name = 'Doenica';
 // PascalCase (recomendado en JavaScript para las clases)
 const LastName = 'sorianne';
 // camelCase (recomendado en JavaScript para las variables y funciones)
 const lastName = 'olifetto';
+document.write(lastame);
+const person = 'John Doe', carName = 'Volvo', price = 200;
+let x; // creando la variable
+x = 5;//asignando valor de tipo numerico entero y valor 5
+x = 'Texto';//asigna valor de tipo string y valor texto
+//infinity
+const variable1 = 1/0;
+//NaN
+const variable2 = parseFloat('prueba');
