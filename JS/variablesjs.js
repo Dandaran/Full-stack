2d1eeffa8d2782a26502bd7fd4887fa529836f07
Dandaran <105198675+Dandaran@users.noreply.github.com>
2022-05-17 12:59:16 +0200

    java(idu)

---
+++ JS/variablesjs.js
@@ -0,0 +1,8 @@
+// kebap-case (no permitido en JavaScript)
+const last-ame = 'Garoppo';
+// snake_case (recomendado en otros lenguajes como Python)
+const last_name = 'Doenica';
+// PascalCase (recomendado en JavaScript para las clases)
+const LastName = 'sorianne';
+// camelCase (recomendado en JavaScript para las variables y funciones)
+const lastName = 'olifetto';
